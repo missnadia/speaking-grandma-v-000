e0021335e5957132d9ede58ad257feac0965577d
Nadia Lee <miss.nadia.lee@gmail.com>
2017-12-19 19:15:20 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if (phrase == (phrase.upcase && "I LOVE YOU GRANDMA!"))
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif (phrase == phrase.upcase)
     puts "NO, NOT SINCE 1938!"
   else
